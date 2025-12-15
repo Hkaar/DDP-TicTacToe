01c67d9866831310ad489824745e2dad3d52c689
subaktikesuma <anjay87666@gmail.com>
2025-12-15 08:25:57 +0800

    Fix Release Build for Windows

--- build.bat
+++ build.bat
@@ -7,11 +7,11 @@ if not exist install mkdir install
 REM Configure with install prefix
 cmake -S . -B build -DCMAKE_INSTALL_PREFIX=./install
 
-REM Build the project
-cmake --build build
+REM Build the project (Release configuration)
+cmake --build build --config Release
 
-REM Install to the install directory
-cmake --install build
+REM Install to the install directory (Release configuration)
+cmake --install build --config Release
 
 echo.
 echo Build complete! Executable is in the install/bin directory.

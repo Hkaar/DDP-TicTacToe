9954fd3a329e104b5e0863985d3c1d1555eb2163
subaktikesuma <anjay87666@gmail.com>
2025-12-22 09:31:21 +0800

    add BGM

--- src/main.c
+++ src/main.c
@@ -2,11 +2,21 @@
 #include <time.h>
 
 #include "ui.h"
+#include "audio.h"
 
 int main(void)
 {
     srand(time(NULL));
 
+    // Initialize audio and play background music
+    if (initAudio()) {
+        playBGM("sounds/bgm.mp3");
+    }
+
     mainMenu();
+
+    // Cleanup audio before exit
+    cleanupAudio();
+
     return 0;
 }
